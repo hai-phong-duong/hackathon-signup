afc55074cfc63c2e5b26ada7ea7be4a71d33d94c
unknown <thomas.duongh@gmail.com>
2025-04-26 17:30:19 -0400

    added content to UserLanding

--- frontend/src/components/UserComponents/UserLanding.jsx
+++ frontend/src/components/UserComponents/UserLanding.jsx
@@ -1,9 +1,22 @@
+import { FileUser } from "lucide-react";
+
 function UserLanding({ auth }) {
     return (
         <div className="bg-bg flex flex-col flex-grow p-35 py-20 pr-50 gap-8">
             <h1 className="text-5xl text-text font-semibold">
                 Welcome, <span className="text-accent">{auth.username}</span>
             </h1>
+            <div className="bg-body p-4 rounded-xl border border-2 border-accent text-accent">
+                <div className="flex justify-between">
+                    <h1 className="text-2xl">Hackathon Status</h1>
+                    <FileUser className="w-6 h-6" />
+                </div>
+                <div>
+                    <p className="text-lg text-text">
+                        ✅ You are successfully registered for TreeHACKS 2025!
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
